53bd781f70fbd6ba72a3e4ef8571d82e1d72f662
BAGUS0630 <mbagus0111@gmail.com>
2025-11-03 21:18:25 +0700

    Add files via upload

---
+++ pertemuan 03/Pertemuan 04/Praktikum/P4_1_714240060/Book_714240060.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714240060
+{
+    public class Book_714240060 : Product_714240060
+    {
+        protected string pageCount;
+
+        public Book_714240060(string type, string title, string pageCount) :  base(type, title)
+        {
+            this.pageCount = pageCount;
+        }
+
+        public string PageCount
+        {
+            get { return pageCount; }
+            set { pageCount = value; }
+        }
+
+        //Implementasi metode abstrak
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Product is a {0} called \"{1}\" and has {2} pages", MyType, MyTitle, pageCount);
+        }
+    }
+}
---
+++ pertemuan 03/Pertemuan 04/Praktikum/P4_1_714240060/Product_714240060.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714240060
+{
+    public abstract class Product_714240060
+    {
+        private string myType;
+        private string myTitle;
+
+        public Product_714240060()
+        {
+
+        }
+
+        //construktor 
+        public Product_714240060(string type, string title)
+        {
+            myType = type;
+            myTitle = title;
+        }
+
+        //property untuk MyType
+        public string MyType
+        {
+            get { return myType; }
+            set { myType = value; }
+        }
+
+        //property untuk MyTitle
+        public string MyTitle
+        {
+            get { return myTitle; }
+            set { myTitle = value; }
+        }
+
+        //metode abstrak untuk menampilkan info produk
+        public abstract void DisplayInfo();
+    }
+}
