433cf4d5e46acbbc03178ff80f401905f4a9d56b
BAGUS0630 <mbagus0111@gmail.com>
2025-11-10 21:20:43 +0700

    Add files via upload

---
+++ Pertemuan 05/Praktikum/P5_2_714240060/P5_2_714240060/ChildForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_2_714240060
+{
+    public partial class ChildForm : Form
+    {
+        private string outputtext = "";
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = outputtext;
+        }
+
+        private void DateOption_CheckedChanged(object sender, EventArgs e)
+        {
+            outputtext = DateTime.Now.ToString("d");
+        }
+
+        private void TimeOption_CheckedChanged(object sender, EventArgs e)
+        {
+            outputtext = DateTime.Now.ToString("t");
+        }
+    }
+}
