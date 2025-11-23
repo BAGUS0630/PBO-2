5ceb0781e1502621898e7858350e9236b2f2f559
BAGUS0630 <mbagus0111@gmail.com>
2025-11-23 12:33:43 +0700

    Add files via upload

---
+++ Pertemuan 06/Tugas/P6_4_714240060/P6_4_714240060/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace P6_4_714240060
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNamaLengkap = new System.Windows.Forms.TextBox();
+            this.txtNoHp = new System.Windows.Forms.TextBox();
+            this.txtKodeVoucher = new System.Windows.Forms.TextBox();
+            this.txtJumlahTiket = new System.Windows.Forms.TextBox();
+            this.txtUlangJumlah = new System.Windows.Forms.TextBox();
+            this.txtIDPemesanan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnPesan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNamaLengkap
+            // 
+            this.txtNamaLengkap.Location = new System.Drawing.Point(399, 130);
+            this.txtNamaLengkap.Name = "txtNamaLengkap";
+            this.txtNamaLengkap.Size = new System.Drawing.Size(166, 22);
+            this.txtNamaLengkap.TabIndex = 0;
+            // 
+            // txtNoHp
+            // 
+            this.txtNoHp.Location = new System.Drawing.Point(399, 183);
+            this.txtNoHp.Name = "txtNoHp";
+            this.txtNoHp.Size = new System.Drawing.Size(166, 22);
+            this.txtNoHp.TabIndex = 0;
+            // 
+            // txtKodeVoucher
+            // 
+            this.txtKodeVoucher.Location = new System.Drawing.Point(399, 236);
+            this.txtKodeVoucher.Name = "txtKodeVoucher";
+            this.txtKodeVoucher.Size = new System.Drawing.Size(166, 22);
+            this.txtKodeVoucher.TabIndex = 0;
+            // 
+            // txtJumlahTiket
+            // 
+            this.txtJumlahTiket.Location = new System.Drawing.Point(399, 291);
+            this.txtJumlahTiket.Name = "txtJumlahTiket";
+            this.txtJumlahTiket.Size = new System.Drawing.Size(166, 22);
+            this.txtJumlahTiket.TabIndex = 0;
+            // 
+            // txtUlangJumlah
+            // 
+            this.txtUlangJumlah.Location = new System.Drawing.Point(399, 349);
+            this.txtUlangJumlah.Name = "txtUlangJumlah";
+            this.txtUlangJumlah.Size = new System.Drawing.Size(166, 22);
+            this.txtUlangJumlah.TabIndex = 0;
+            // 
+            // txtIDPemesanan
+            // 
+            this.txtIDPemesanan.Location = new System.Drawing.Point(399, 87);
+            this.txtIDPemesanan.Name = "txtIDPemesanan";
+            this.txtIDPemesanan.Size = new System.Drawing.Size(166, 22);
+            this.txtIDPemesanan.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pemesanan Tiket Konser ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Pemesanan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nama Lengkap";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nomor HP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Kode Voucher";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Jumlah Tiket";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 351);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(189, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Konfirmasi Jumlah Tiket";
+            // 
+            // btnPesan
+            // 
+            this.btnPesan.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPesan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesan.Location = new System.Drawing.Point(226, 409);
+            this.btnPesan.Name = "btnPesan";
+            this.btnPesan.Size = new System.Drawing.Size(183, 29);
+            this.btnPesan.TabIndex = 3;
+            this.btnPesan.Text = "Pesan Sekarang";
+            this.btnPesan.UseVisualStyleBackColor = false;
+            this.btnPesan.Click += new System.EventHandler(this.btnPesan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.btnPesan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUlangJumlah);
+            this.Controls.Add(this.txtJumlahTiket);
+            this.Controls.Add(this.txtKodeVoucher);
+            this.Controls.Add(this.txtNoHp);
+            this.Controls.Add(this.txtIDPemesanan);
+            this.Controls.Add(this.txtNamaLengkap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNamaLengkap;
+        private System.Windows.Forms.TextBox txtNoHp;
+        private System.Windows.Forms.TextBox txtKodeVoucher;
+        private System.Windows.Forms.TextBox txtJumlahTiket;
+        private System.Windows.Forms.TextBox txtUlangJumlah;
+        private System.Windows.Forms.TextBox txtIDPemesanan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnPesan;
+    }
+}
+
---
+++ Pertemuan 06/Tugas/P6_4_714240060/P6_4_714240060/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_714240060
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ------------------------------------------------------------------
+        // A. CHAR TEXTBOX & NUMERIC TEXTBOX (MENGGUNAKAN KeyPress EVENT)
+        // ------------------------------------------------------------------
+
+        // Numeric Textbox (txtNoHP): Hanya mengizinkan digit
+        private void txtNoHP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Izinkan digit (0-9) dan tombol kontrol (seperti Backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Abaikan input non-digit
+            }
+        }
+
+        // Char Textbox (txtNamaLengkap): Hanya mengizinkan huruf dan spasi
+        private void txtNamaLengkap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Izinkan huruf, spasi, dan tombol kontrol
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ' ')
+            {
+                e.Handled = true; // Abaikan input non-huruf/spasi
+            }
+        }
+
+        // ------------------------------------------------------------------
+        // B. UPPER CASE TEXTBOX & LOWER CASE TEXTBOX (MENGGUNAKAN TextChanged EVENT)
+        // ------------------------------------------------------------------
+
+        // Upper Case Textbox (txtIDPemesanan)
+        private void txtIDPemesanan_TextChanged(object sender, EventArgs e)
+        {
+            // Simpan posisi kursor
+            int cursor = txtIDPemesanan.SelectionStart;
+            txtIDPemesanan.Text = txtIDPemesanan.Text.ToUpper();
+            txtIDPemesanan.SelectionStart = cursor;
+        }
+
+        // Lower Case Textbox (txtNamaLengkap)
+        private void txtNamaLengkap_TextChanged(object sender, EventArgs e)
+        {
+            // Simpan posisi kursor
+            int cursor = txtNamaLengkap.SelectionStart;
+            txtNamaLengkap.Text = txtNamaLengkap.Text.ToLower();
+            txtNamaLengkap.SelectionStart = cursor;
+        }
+
+        // ------------------------------------------------------------------
+        // C. SEMUA VALIDASI PADA TOMBOL SUBMIT (Click EVENT)
+        // ------------------------------------------------------------------
+
+        private void btnPesan_Click(object sender, EventArgs e)
+        {
+            // 1. REQUIRED VALIDATOR
+            if (string.IsNullOrWhiteSpace(txtIDPemesanan.Text) ||
+                string.IsNullOrWhiteSpace(txtNamaLengkap.Text) ||
+                string.IsNullOrWhiteSpace(txtNoHp.Text) ||
+                string.IsNullOrWhiteSpace(txtKodeVoucher.Text) ||
+                string.IsNullOrWhiteSpace(txtJumlahTiket.Text) ||
+                string.IsNullOrWhiteSpace(txtUlangJumlah.Text))
+            {
+                MessageBox.Show("Semua kolom wajib diisi.", "Validasi Gagal - Required", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 2. LENGTH VALIDATOR (ID Pemesanan harus 5 karakter)
+            if (txtIDPemesanan.Text.Length != 5)
+            {
+                MessageBox.Show("ID Pemesanan harus tepat 5 karakter.", "Validasi Gagal - Length", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtIDPemesanan.Focus();
+                return;
+            }
+
+            // 3. REGEX VALIDATOR (Kode Voucher: Pola DISKON diikuti 3 Angka)
+            string regexVoucher = @"^DISKON\d{3}$";
+            if (!Regex.IsMatch(txtKodeVoucher.Text, regexVoucher))
+            {
+                MessageBox.Show("Kode Voucher tidak valid. Gunakan format DISKONxxx (contoh: DISKON123).", "Validasi Gagal - Regex", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtKodeVoucher.Focus();
+                return;
+            }
+
+            // 4. Pengecekan Numeric dan Comparison (Min/Max)
+            if (!int.TryParse(txtJumlahTiket.Text, out int jumlahTiket) || jumlahTiket < 1 || jumlahTiket > 10)
+            {
+                MessageBox.Show("Jumlah Tiket harus angka antara 1 sampai 10.", "Validasi Gagal - Comparison", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtJumlahTiket.Focus();
+                return;
+            }
+
+            // 5. COMPARISON VALIDATOR (Konfirmasi Jumlah Tiket harus sama)
+            if (txtJumlahTiket.Text != txtUlangJumlah.Text)
+            {
+                MessageBox.Show("Konfirmasi Jumlah Tiket tidak cocok.", "Validasi Gagal - Comparison", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUlangJumlah.Focus();
+                return;
+            }
+
+
+            // ------------------------------------------------------------------
+            // D. TAMPILKAN ISIAN FORM DALAM MESSAGE BOX
+            // ------------------------------------------------------------------
+            string hasilPemesanan = $"--- Data Pemesanan Tiket Konser ---\n\n" +
+                                    $"ID Pemesanan: {txtIDPemesanan.Text}\n" +
+                                    $"Nama Pembeli: {txtNamaLengkap.Text}\n" +
+                                    $"Nomor HP: {txtNoHp.Text}\n" +
+                                    $"Kode Voucher: {txtKodeVoucher.Text}\n" +
+                                    $"Jumlah Tiket: {txtJumlahTiket.Text} buah\n\n" +
+                                    $"Status: Pemesanan Tiket Berhasil!";
+
+            MessageBox.Show(hasilPemesanan, "Sukses Submit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
