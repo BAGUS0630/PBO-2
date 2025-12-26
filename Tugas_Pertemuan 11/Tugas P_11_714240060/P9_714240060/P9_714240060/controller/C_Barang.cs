7f64f161dadde26c6cd31a1adaf9626cb2d1ac63
BAGUS0630 <mbagus0111@gmail.com>
2025-12-26 16:28:22 +0700

    Initial commit - Tugas Pertemmuan 11

---
+++ Tugas_Pertemuan 11/Tugas P_11_714240060/P9_714240060/P9_714240060/controller/C_Barang.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_714240060.controller
+{
+    internal class C_Barang
+    {
+    }
+}
