13810d72792d024c35dc33306597c2898a12e6c9
BAGUS0630 <mbagus0111@gmail.com>
2026-01-05 15:18:22 +0700

    prak 13

---
+++ Pertemuan 13/Praktikum/P9_714240060/P9_714240060/view/ParentForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240060.view
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataMahasiswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 FormMhs = new Form1();
+            FormMhs.MdiParent = this;
+            FormMhs.Show();
+        }
+
+        private void dataNilaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNilai FrmNilai = new FormNilai();
+            FrmNilai.MdiParent = this;
+            FrmNilai.Show();
+        }
+
+        private void dataMasterBarangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // cek apakah Form_Barang sudah terbuka
+            foreach (Form f in this.MdiChildren)
+            {
+                if (f is Form_Barang)
+                {
+                    f.Activate();
+                    return;
+                }
+            }
+
+            // jika belum terbuka
+            Form_Barang frm = new Form_Barang();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+
+        private void tugasPraktikum11ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataTransaksiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormTransaksi frmTrans = new FormTransaksi();
+            frmTrans.MdiParent = this; // Menjadikan form muncul di dalam ParentForm
+            frmTrans.Show();
+
+        }
+
+        private void ParentForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
